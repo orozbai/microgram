53f832bb4ab372f78dc56d5876261f5a75802d81
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-03-16 00:16:12 +0600

    added method to create table and insert into table

--- src/main/java/com/example/microgram/service/PublicationService.java
+++ src/main/java/com/example/microgram/service/PublicationService.java
@@ -5,12 +5,56 @@ import com.example.microgram.entity.Publication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 @Service
 public class PublicationService {
     @Autowired
     private PublicationDao publicationDao;
+
+    private Connection connection;
+
+    private Connection getNewConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=orozbay233";
+        return DriverManager.getConnection(url);
+    }
+
+    private void init() throws SQLException {
+        connection = getNewConnection();
+    }
+
+    private void executeUpdate(String query) throws SQLException {
+        init();
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+    }
+
+    private void createPublicationTable() throws SQLException {
+        String customerTableQuery = "create table publications (" +
+                "id serial primary key," +
+                "imageLink varchar(400)," +
+                "description varchar(400)," +
+                "publicationDateTime TIMESTAMP," +
+                "likes integer)";
+        String customerEntryQuery = "insert into publications(id, imageLink, description, publicationDateTime, likes)" +
+                "values(1, 'https://www.example.com/png.1', 'some desc..', '2022-05-01 12:30:00', 4)";
+        executeUpdate(customerTableQuery);
+        executeUpdate(customerEntryQuery);
+    }
+
+    public String shouldCreateTablePublication() {
+        try {
+            createPublicationTable();
+            connection.createStatement().execute("select * from publication");
+            return "All it's OK";
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+    }
 
     public List<Publication> getPublicationOfOtherUsers() {
         return publicationDao.getPublicationOfOtherUsers();
